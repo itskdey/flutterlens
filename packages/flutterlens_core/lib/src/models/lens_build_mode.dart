33642d54b2e1898b02814833e9cee80573bb1475
Mean Pheakdey <80913151+itskdey@users.noreply.github.com>
2026-08-23 13:49:50 +0700

    style: stabilize Phase 2 model formatting

--- packages/flutterlens_core/lib/src/models/lens_build_mode.dart
+++ packages/flutterlens_core/lib/src/models/lens_build_mode.dart
@@ -3,9 +3,11 @@ enum LensBuildMode {
   profile,
   unknown;
 
-  String get label => switch (this) {
-    LensBuildMode.debug => 'Debug',
-    LensBuildMode.profile => 'Profile',
-    LensBuildMode.unknown => 'Unknown',
-  };
+  String get label {
+    return switch (this) {
+      LensBuildMode.debug => 'Debug',
+      LensBuildMode.profile => 'Profile',
+      LensBuildMode.unknown => 'Unknown',
+    };
+  }
 }
